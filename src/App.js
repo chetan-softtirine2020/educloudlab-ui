3f2f6123b5d98f89733d91d5ac35f691480c8af7
chetan-softtirine2020 <chetan.softtrine@gmail.com>
2021-09-30 12:54:15 +0530

    Appicalition Demo

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <table>
+          <tr>
+            <th>Sr No. </th>
+            <th>Name </th>
+            <th>Action </th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Sanjay Vitkare</td>
+            <td>
+              <a href="https://educloud-meet.com/GroupMeeting">
+                <button type="button">Join</button>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Gopal Tayde</td>
+            <td>
+              <a href="https://educloud-meet.com/GroupMeeting">
+                <button type="button">Join</button>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Sanjay Adgale</td>
+            <td>
+              <a href="https://educloud-meet.com/GroupMeeting">
+                <button type="button">Join</button>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Chetan Barde</td>
+            <td>
+              <a href="https://educloud-meet.com/GroupMeeting">
+                <button type="button">Join</button>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Dipali Patil</td>
+            <td>
+              <a href="https://educloud-meet.com/GroupMeeting">
+                <button type="button">Join</button>
+              </a>
+            </td>
+          </tr>
+            <tr>
+            <td>6</td>
+            <td>Ajit</td>
+            <td>
+              <a href="https://educloud-meet.com/GroupMeeting">
+                <button type="button">Join</button>
+              </a>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
